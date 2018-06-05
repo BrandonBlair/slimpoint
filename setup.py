9b5c29dd0bb4b509709715b5c273fd50c37813c6
Brandon Blair <brandon.blair@rewardstyle.com>
2018-06-05 14:32:38 -0500

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='slimpoint',
-    version='1.0.2',
+    version='1.0.3',
     description='Slim, readable, expressive interactions with backend services',
     author='Brandon Blair',
     author_email='cbrandon.blair@gmail.com',
